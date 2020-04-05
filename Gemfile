85aba290ed06c759865ea86805f890ae9df53860
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-05 20:09:13 +0900

    堀メンターのレビュー内容に対応

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :development, :test do
   gem 'webdrivers'
   gem 'launchy'
   gem 'database_cleaner'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,12 +5,15 @@ RSpec.describe "helpers/application_helper.rb", type: :view do
     it "文字列を引数に取ると、サイト名と結合した文字列を返す" do
       expect(full_title('bags')).to eq 'bags | BIGBAG store'
     end
+
     it "数字を引数に取ると、サイト名と結合した文字列を返す" do
       expect(full_title(123)).to eq '123 | BIGBAG store'
     end
+
     it "nilを引数に取ると、サイト名のみの文字列を返す" do
       expect(full_title(nil)).to eq 'BIGBAG store'
     end
+
     it "空白文字を引数に取ると、サイト名のみの文字列を返す" do
       expect(full_title('')).to eq 'BIGBAG store'
     end
