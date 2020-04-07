712c4b10dd6303274cb06a9c03676db2ae59eb31
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-07 10:36:47 +0900

    specの内容を可読性の高い表示に修正

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
-  let!(:product) { FactoryBot.create(:spree_product) }
-
-  it '#show' do
+  let(:product) { FactoryBot.create(:spree_product) }
+  before 'showアクションを起動' do
     get :show, params: { id: product.id }
-    expect(response).to render_template :show
-    expect(assigns(:product)).to eq product
-    expect(response).to be_success
+  end
+
+  describe '#show' do
+    it '@product変数はSpree::Productクラスのインスタンス変数である' do
+      expect(assigns(:product)).to eq product
+    end
+
+    it 'products/show.html.erbがレンダリングされる' do
+      expect(response).to render_template :show
+    end
+
+    it 'ビューのレンダリングに成功する' do
+      expect(response).to be_successful
+    end
   end
 end
--- spec/views/potepan/products/show.html.erb_spec.rb
+++ spec/views/potepan/products/show.html.erb_spec.rb
@@ -2,13 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "products/show.html.erb", type: :view do
   let(:product) { FactoryBot.create(:spree_product) }
-
-  it '正常なビューを返す' do
+  before '/potepan/products/1にGETリクエストを送付' do
     visit potepan_product_path(product.id)
+  end
 
-    expect(page).to have_title "#{product.name} | BIGBAG store"
-    expect(page).to have_content product.name
-    expect(page).to have_content product.price.round
-    expect(page).to have_content product.description
+  describe 'products/show.html.erbを表示' do
+    it 'タイトルに@productのname属性が表示されている' do
+      expect(page).to have_title "#{product.name} | BIGBAG store"
+    end
+
+    it 'ボディに@productのname属性が表示されている' do
+      expect(page).to have_content product.name
+    end
+
+    it 'ボディに@productのprice属性が表示されている' do
+      expect(page).to have_content product.price.round
+    end
+
+    it 'ボディに@productのdescription属性が表示されている' do
+      expect(page).to have_content product.description
+    end
   end
 end
