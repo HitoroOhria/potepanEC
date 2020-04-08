45d6e01b71913a4ef0b931937987b55fbece1eb3
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-08 15:57:17 +0900

    potepan/categories/show.html.erbのpanel表示部分を、動的に表示するように修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,5 +1,21 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    @category = Spree::Taxonomy.find(params[:id])
+    if category_id_is_taxonomy?
+      @category = Spree::Taxonomy.find(params[:id])
+      @products = Spree::Taxon.new.products
+      @category.taxons.each do |taxon|
+        @products.append(taxon.products)
+      end
+    else
+      taxon_id = params[:id].to_i - Spree::Taxonomy.count
+      @category = Spree::Taxon.find(taxon_id)
+      @products = @category.products
+    end
   end
+
+  private
+
+    def category_id_is_taxonomy?
+      params[:id].to_i <= Spree::Taxonomy.count ? true : false
+    end
 end
---
+++ spec/helpers/potepan/categories_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "potepan/categories_helper.rb", type: :helper do
+  # let!(:taxon)
+  # before do
+  #   3.times do
+  #
+  #   end
+  # end
+  # describe 'take_all_taxonomies' do
+  #   it '全てのTxonomyクラスのモデルオブジェクトを返す' do
+  #     expect(take_all_taxonomies).to eq Spree::Taxonomy.all
+  #   end
+  # end
+  #
+  # describe 'take_option_type(option_type_name)' do
+  #   it '引数に与えられた文字列のOptionTypeを返す' do
+  #     expect(take_option_type('color')).to eq
+  #   end
+  # end
+end
