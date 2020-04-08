823fb00d74a6d7da2729f88b37d37926bfcdb38b
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-08 11:31:32 +0900

    specのテスト名をより適切な内容に修正

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
   let(:product) { FactoryBot.create(:spree_product) }
-  before 'showアクションを起動' do
+  before do
     get :show, params: { id: product.id }
   end
 
   describe '#show' do
-    it '@product変数はSpree::Productクラスのインスタンス変数である' do
-      expect(assigns(:product)).to eq product
+    it '@product変数は、id属性がparams[:id]に対応したSpree::Productモデルオブジェクトである' do
+      expect(assigns(:product)).to eq Spree::Product.find(product.id)
     end
 
-    it 'products/show.html.erbがレンダリングされる' do
+    it 'HTTPレスポンスステータスコードが200である' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'レスポンスののテンプレートがproducts/show.html.erbである' do
       expect(response).to render_template :show
-    end
-
-    it 'ビューのレンダリングに成功する' do
-      expect(response).to be_successful
     end
   end
 end
--- spec/views/potepan/products/show.html.erb_spec.rb
+++ spec/views/potepan/products/show.html.erb_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "products/show.html.erb", type: :view do
   let(:product) { FactoryBot.create(:spree_product) }
-  before '/potepan/products/1にGETリクエストを送付' do
+  before do
     visit potepan_product_path(product.id)
   end
 
   describe 'products/show.html.erbを表示' do
-    it 'タイトルに@productのname属性が表示されている' do
-      expect(page).to have_title "#{product.name} | BIGBAG store"
+    it 'titleがfull_title(@product.name)と一致する' do
+      expect(page.title).to eq full_title(product.name)
     end
 
-    it 'ボディに@productのname属性が表示されている' do
+    it 'bodyに@productのname属性が表示されている' do
       expect(page).to have_content product.name
     end
 
-    it 'ボディに@productのprice属性が表示されている' do
+    it 'bodyに@productのprice属性が表示されている' do
       expect(page).to have_content product.price.round
     end
 
-    it 'ボディに@productのdescription属性が表示されている' do
+    it 'bodyに@productのdescription属性が表示されている' do
       expect(page).to have_content product.description
     end
   end
