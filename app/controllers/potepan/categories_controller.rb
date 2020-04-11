da93fe64a9b2c3ef25394320514c8a2f5bfd2a09
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-11 09:58:43 +0900

    categories_controller内の変数をより適切な変数名に修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,21 +1,19 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    if category_id_is_taxonomy?
-      @category = Spree::Taxonomy.find(params[:id])
+    @taxon = Spree::Taxon.find(params[:id])
+    if taxon_id_is_taxonomy?
       @products = Spree::Taxon.new.products
-      @category.taxons.each do |taxon|
+      @taxon.taxonomy.taxons.each do |taxon|
         @products.append(taxon.products)
       end
     else
-      taxon_id = params[:id].to_i - Spree::Taxonomy.count
-      @category = Spree::Taxon.find(taxon_id)
       @products = @category.products
     end
   end
 
   private
 
-    def category_id_is_taxonomy?
-      params[:id].to_i <= Spree::Taxonomy.count ? true : false
+    def taxon_id_is_taxonomy?
+      params[:id].to_i <= Spree::Taxonomy.count
     end
 end
