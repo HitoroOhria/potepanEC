806fd7adc54ecf1d39baeb7a32c546b17c7bd442
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-16 13:34:47 +0900

    categories_controller.rbの@productsの収集方法をイーガーロードに変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,13 +1,19 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
-    if @taxon.taxonomy.present?
+    if taxon_id_is_taxonomy?
       @products = Spree::Taxon.new.products
-      @taxon.taxonomy.taxons.each do |taxon|
+      Spree::Taxon.eager_load(:products).where(taxonomy_id: @taxon.taxonomy.id).each do |taxon|
         @products.push(taxon.products)
       end
     else
       @products = @taxon.products
     end
+  end
+
+  private
+
+  def taxon_id_is_taxonomy?
+    Spree::Taxonomy.find_by(name: @taxon.name)
   end
 end
