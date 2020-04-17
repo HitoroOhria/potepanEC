14c6a1547b98ce4d5b2e1302deb633232edcae26
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-17 20:29:37 +0900

    paperclip.rbの設定を修正

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,3 @@
-Spree::Image.attachment_definitions[:attachment] = {
-  url:         "stylishcoffee-#{Rails.env}.s3.amazonaws.com",
-  default_url: '/spree/products/noimage/:style.png',
-  path:        '/spree/products/:id/:style/:basename.:extension'
-}
+Spree::Image.attachment_definitions[:attachment][:url]         = "stylishcoffee-#{Rails.env}.s3.amazonaws.com"
+Spree::Image.attachment_definitions[:attachment][:default_url] = '/spree/products/noimage/:style.png'
+Spree::Image.attachment_definitions[:attachment][:path]        = '/spree/products/:id/:style/:basename.:extension'
