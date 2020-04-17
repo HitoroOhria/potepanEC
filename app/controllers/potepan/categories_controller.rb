e3621bf67632543eb29f2243b443e31c51cecb39
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-17 16:15:50 +0900

    categories_controller.rbのロジックを、awesome_nested_setを使用したものに修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,19 +1,13 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
-    if taxon_id_is_taxonomy?
+    if @taxon.leaf?
+      @products = @taxon.products
+    else
       @products = Spree::Taxon.new.products
-      Spree::Taxon.includes(:products).where(taxonomy_id: @taxon.taxonomy.id).each do |taxon|
+      Spree::Taxon.includes(:products).where(name: @taxon.self_and_descendants.pluck(:name)).each do |taxon|
         @products.push(taxon.products)
       end
-    else
-      @products = @taxon.products
     end
-  end
-
-  private
-
-  def taxon_id_is_taxonomy?
-    Spree::Taxonomy.find_by(name: @taxon.name)
   end
 end
