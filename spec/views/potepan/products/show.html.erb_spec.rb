9cf36da93437b905fba5dc026fb27f64ce43cad5
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-19 11:18:38 +0900

    商品詳細ページの関連商品の、スペックの枠組みを修正

--- spec/views/potepan/products/show.html.erb_spec.rb
+++ spec/views/potepan/products/show.html.erb_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "products/show.html.erb", type: :view do
-  let(:product) { create(:product) }
+  let(:taxon1)         { create(:taxon) }
+  let(:taxon1_product) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+
   before do
-    product.taxons.create(attributes_for(:taxon))
-    visit potepan_product_path(product.id)
+    visit potepan_product_path(taxon1_product.id)
   end
+
+  subject { page }
 
   describe 'GET potepan/products/:product_id' do
     it 'titleがfull_title(@product.name)と一致する' do
@@ -25,34 +28,54 @@ RSpec.describe "products/show.html.erb", type: :view do
     end
 
     context '商品の所属するカテゴリーが1つの時'do
-      context 'そのカテゴリーに所属する商品の個数が4個未満の場合' do
-        it '関連商品に、そのカテゴリーに所属する商品を全て表示する' do
+      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+      let!(:taxon1_other_product2) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
 
+      context 'そのカテゴリーに所属する商品の個数が3個の場合' do
+        it '関連商品には、自身の商品を表示しない' do
+          should_not have_css('h5', text: taxon1_product.name)
+        end
+
+        it '関連商品に自身を除いた2個の商品を表示する' do
+          should have_css('.productBox', count: taxon1.products.count)
         end
       end
 
-      context 'そのカテゴリーに所属する商品の個数が5個以上の場合' do
-        it '関連商品に、そのカテゴリーに所属する商品を4個まで表示する' do
+      context 'そのカテゴリーに所属する商品の個数が6個の場合' do
+        let!(:taxon1_other_product3) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+        let!(:taxon1_other_product4) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
 
+        it '関連商品には、自身の商品を表示しない' do
+          should_not have_css('h5', text: taxon1_product.name)
+        end
+
+        it '関連商品に、自身の商品を除いた4個を表示する' do
+          should have_css('.productBox', count: 4)
         end
       end
     end
 
     context '商品の所属するカテゴリーが2つの時'do
-      context '２つのカテゴリーに所属する商品の合計個数が4個未満の場合' do
-        it '関連商品に、2つのカテゴリーに所属する商品を全て表示する' do
+      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+      let(:taxon2) { taxon1_product.taxons.create(attribute_for(:taxon)) }
+      let!(:taxon2_other_product1) { taxon2.products.create(attributes_for(:product, shipping_category: 1)) }
+      let!(:taxon2_other_product2) { taxon2.products.create(attributes_for(:product, shipping_category: 1)) }
+
+
+      context '２つのカテゴリーに所属する商品の合計個数が5個未満の場合' do
+        it '関連商品に、2つのカテゴリーに所属する自身の商品以外の個数分、表示する' do
 
         end
       end
 
       context '2つのカテゴリーの商品の合計個数が5個以上の場合' do
-        it '関連商品に、2つのカテゴリーに所属する商品を4個まで表示する' do
+        it '関連商品に、2つのカテゴリーに所属する自身の商品以外を4個表示する' do
 
         end
       end
     end
 
-    context '商品の所属するカテゴリーが2つで、2つ目のカテゴリーが子カテゴリーを一つ持つ(ネストされている)時'do
+    context '商品の所属するカテゴリーが2つで、2つ目のカテゴリーが子カテゴリーを一つ持つ(ネストしている)時'do
       it '関連商品は、子カテゴリーの商品も含め、上限4個まで商品を表示する' do
 
       end
