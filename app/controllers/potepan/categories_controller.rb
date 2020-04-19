7e4bdef2011b581de3f97b8ca96b1c9fbfbfa34f
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-19 17:41:48 +0900

    taxonの商品一覧を取得する.all_productsを使用

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,14 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
-    if @taxon.leaf?
-      @products = @taxon.products
-    else
-      @products = Spree::Taxon.new.products
-      Spree::Taxon.includes(:products)
-                  .where(name: @taxon.self_and_descendants.pluck(:name)).each do |taxon|
-        @products.push(taxon.products)
-      end
-    end
+    @products = @taxon.leaf? ? @taxon.products : @taxon.all_products
   end
 end
