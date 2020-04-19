567ae44daa53f2692c1d89761506ad294b28b8cd
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-19 17:03:38 +0900

    商品詳細画面の、関連商品のテストを記述

--- spec/views/potepan/products/show.html.erb_spec.rb
+++ spec/views/potepan/products/show.html.erb_spec.rb
@@ -2,82 +2,121 @@ require 'rails_helper'
 
 RSpec.describe "products/show.html.erb", type: :view do
   let(:taxon1)         { create(:taxon) }
-  let(:taxon1_product) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+  let(:taxon1_product) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
-  before do
-    visit potepan_product_path(taxon1_product.id)
-  end
+  before { visit potepan_product_path(taxon1_product.id) }
 
   subject { page }
 
   describe 'GET potepan/products/:product_id' do
     it 'titleがfull_title(@product.name)と一致する' do
-      expect(page.title).to eq full_title(product.name)
+      expect(page.title).to eq full_title(taxon1_product.name)
     end
 
     it 'bodyに@productのname属性が表示されている' do
-      expect(page).to have_content product.name
+      expect(page).to have_content taxon1_product.name
     end
 
     it 'bodyに@productのprice属性が表示されている' do
-      expect(page).to have_content product.price.round
+      expect(page).to have_content taxon1_product.price.round
     end
 
     it 'bodyに@productのdescription属性が表示されている' do
-      expect(page).to have_content product.description
+      expect(page).to have_content taxon1_product.description
     end
 
     context '商品の所属するカテゴリーが1つの時'do
-      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
-      let!(:taxon1_other_product2) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
+      let!(:taxon1_other_product2) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
+
+      before { visit potepan_product_path(taxon1_product.id) }
 
       context 'そのカテゴリーに所属する商品の個数が3個の場合' do
-        it '関連商品には、自身の商品を表示しない' do
+        it '関連商品は、自身の商品を表示しない' do
           should_not have_css('h5', text: taxon1_product.name)
         end
 
-        it '関連商品に自身を除いた2個の商品を表示する' do
-          should have_css('.productBox', count: taxon1.products.count)
+        it '関連商品は、自身を除いた2個の商品を表示する' do
+          should have_css('.productBox', text: taxon1_other_product1.name)
+          should have_css('.productBox', text: taxon1_other_product2.name)
         end
       end
 
-      context 'そのカテゴリーに所属する商品の個数が6個の場合' do
-        let!(:taxon1_other_product3) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
-        let!(:taxon1_other_product4) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
+      context 'そのカテゴリーに所属する商品の個数が5個の場合' do
+        let!(:taxon1_other_product3) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon1_other_product4) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
-        it '関連商品には、自身の商品を表示しない' do
+        before { visit potepan_product_path(taxon1_product.id) }
+
+        it '関連商品は、自身の商品を表示しない' do
           should_not have_css('h5', text: taxon1_product.name)
         end
 
-        it '関連商品に、自身の商品を除いた4個を表示する' do
-          should have_css('.productBox', count: 4)
+        it '関連商品は、自身の商品を除いた4個を表示する' do
+          should have_css('.productBox', text: taxon1_other_product1.name)
+          should have_css('.productBox', text: taxon1_other_product2.name)
+          should have_css('.productBox', text: taxon1_other_product3.name)
+          should have_css('.productBox', text: taxon1_other_product4.name)
         end
       end
     end
 
     context '商品の所属するカテゴリーが2つの時'do
-      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category: 1)) }
-      let(:taxon2) { taxon1_product.taxons.create(attribute_for(:taxon)) }
-      let!(:taxon2_other_product1) { taxon2.products.create(attributes_for(:product, shipping_category: 1)) }
-      let!(:taxon2_other_product2) { taxon2.products.create(attributes_for(:product, shipping_category: 1)) }
+      let!(:taxon2) { taxon1_product.taxons.create(attributes_for(:taxon)) }
+      let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
+      let!(:taxon2_other_product2) { taxon2.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
+      before { visit potepan_product_path(taxon1_product.id) }
 
-      context '２つのカテゴリーに所属する商品の合計個数が5個未満の場合' do
-        it '関連商品に、2つのカテゴリーに所属する自身の商品以外の個数分、表示する' do
+      context '2つのカテゴリーに所属する商品の合計個数が3個の場合' do
+        it '関連商品は、自身の商品を表示しない' do
+          should_not have_css('h5', text: taxon1_product.name)
+        end
 
+        it '関連商品は、自身を除いた2個の商品を表示する' do
+          should have_css('.productBox', text: taxon1_other_product1.name)
+          should have_css('.productBox', text: taxon2_other_product2.name)
         end
       end
 
-      context '2つのカテゴリーの商品の合計個数が5個以上の場合' do
-        it '関連商品に、2つのカテゴリーに所属する自身の商品以外を4個表示する' do
+      context '2つのカテゴリーに所属する商品の合計個数が5個の場合' do
+        let!(:taxon2_other_product3) { taxon2.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon2_other_product4) { taxon2.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
+        before { visit potepan_product_path(taxon1_product.id) }
+
+        it '関連商品は、自身の商品を表示しない' do
+          should_not have_css('h5', text: taxon1_product.name)
+        end
+
+        it '関連商品は、自身の商品を除いた4個を表示する' do
+          should have_css('.productBox', text: taxon1_other_product1.name)
+          should have_css('.productBox', text: taxon2_other_product2.name)
+          should have_css('.productBox', text: taxon2_other_product3.name)
+          should have_css('.productBox', text: taxon2_other_product4.name)
         end
       end
     end
 
     context '商品の所属するカテゴリーが2つで、2つ目のカテゴリーが子カテゴリーを一つ持つ(ネストしている)時'do
-      it '関連商品は、子カテゴリーの商品も含め、上限4個まで商品を表示する' do
+      context '3つのカテゴリーの商品の合計個数が4個の場合' do
+        let!(:taxon2) { taxon1_product.taxons.create(attributes_for(:taxon, taxonomy_id: taxon1.taxonomy.id)) }
+        let!(:taxon3) { taxon2.children.create(attributes_for(:taxon, taxonomy_id: taxon2.taxonomy.id)) }
+        let!(:taxon1_other_product1) { taxon1.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon2_other_product2) { taxon2.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon3_other_product3) { taxon2.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
+        before { visit potepan_product_path(taxon1_product.id) }
+
+        it '関連商品は、自身の商品を表示しない' do
+          should_not have_css('h5', text: taxon1_product.name)
+        end
+
+        it '関連商品は、子カテゴリーの商品も含め、3個の商品を表示する' do
+          should have_css('.productBox', text: taxon1_other_product1.name)
+          should have_css('.productBox', text: taxon2_other_product2.name)
+          should have_css('.productBox', text: taxon3_other_product3.name)
+        end
       end
     end
   end
