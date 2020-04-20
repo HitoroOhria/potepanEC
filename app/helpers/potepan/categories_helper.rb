b15319018c9bdd65a05da0b72c82b70c42f0c5ae
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-20 20:30:33 +0900

    ビューのロジックをproduct_includes_table_where(products)に移動

--- app/helpers/potepan/categories_helper.rb
+++ app/helpers/potepan/categories_helper.rb
@@ -2,4 +2,8 @@ module Potepan::CategoriesHelper
   def option_values(option_type_name)
     Spree::OptionType.includes(:option_values).find_by(name: option_type_name).option_values
   end
+
+  def product_includes_table_where(products)
+    Spree::Product.includes(master: %i[images default_price]).where(name: products.pluck(:name))
+  end
 end
