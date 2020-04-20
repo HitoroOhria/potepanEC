f0b9491df7a04a9c40be9df21c3ce3a003912c18
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-20 20:55:39 +0900

    ビューのロジックをimage_product_url(product)に移動

--- app/helpers/potepan/categories_helper.rb
+++ app/helpers/potepan/categories_helper.rb
@@ -6,4 +6,8 @@ module Potepan::CategoriesHelper
   def product_includes_table_where(products)
     Spree::Product.includes(master: %i[images default_price]).where(name: products.pluck(:name))
   end
+
+  def image_product_url(product)
+    product.images.empty? ? Spree::Image.new.url(:product) : product.images.first.url(:product)
+  end
 end
