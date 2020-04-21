03ffbe4dbb6ba474c0550c6ceabbcde0152a2481
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-21 10:10:43 +0900

    categories_controller.rbの不要さ三項演算子を削除

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
-    @products = @taxon.leaf? ? @taxon.products : @taxon.all_products
+    @products = @taxon.all_products
   end
 end
