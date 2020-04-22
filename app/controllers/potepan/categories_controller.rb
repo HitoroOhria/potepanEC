5ffeb3d1d20bff3781b2ec0314a17f7aabb8bccc
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-22 11:05:54 +0900

    show.html.erbのSpree::Taxonomy.includes(:taxons).allを、categories_controller.rbに移動

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
     @products = @taxon.all_products
+    @taxonomies = Spree::Taxonomy.includes(:taxons).all
   end
 end
