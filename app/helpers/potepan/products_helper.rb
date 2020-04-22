849c503282a56081a02da23d08e1287c8eaa2d33
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-22 11:06:42 +0900

    一覧ペジへ戻るのリンクを、product_to_category_path(product)に修正

--- app/helpers/potepan/products_helper.rb
+++ app/helpers/potepan/products_helper.rb
@@ -1,12 +1,17 @@
 module Potepan::ProductsHelper
   def render_carousel_image(product, carousel_type: :large)
-    if product.images.empty?
+    if product.images.present?
+      render product.images,
+             carousel_type: carousel_type
+    else
       render 'potepan/spree/images/image',
              carousel_type: carousel_type,
              image_counter: 0, image: Spree::Image.new
-    else
-      render product.images,
-             carousel_type: carousel_type
     end
+  end
+
+  def product_to_category_path(product)
+    product.taxons.present? ? potepan_category_path(product.taxons.first.id)
+                            : potepan_category_path(1)
   end
 end
