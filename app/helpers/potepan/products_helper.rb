f04e5b381eb27d345250c8d2d42b29390ecd8561
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-22 11:36:57 +0900

    _image.html.erbをpotepan/sharedに移動、render_carousel_image(product, carousel_type: :large)修正

--- app/helpers/potepan/products_helper.rb
+++ app/helpers/potepan/products_helper.rb
@@ -1,12 +1,15 @@
 module Potepan::ProductsHelper
   def render_carousel_image(product, carousel_type: :large)
-    if product.images.present?
-      render product.images,
-             carousel_type: carousel_type
+    images = product.images
+    if images.present?
+      render partial:    'potepan/shared/image',
+             collection: images,
+             locals:     { carousel_type: carousel_type }
     else
-      render 'potepan/spree/images/image',
-             carousel_type: carousel_type,
-             image_counter: 0, image: Spree::Image.new
+      render 'potepan/shared/image',
+             image:         Spree::Image.new,
+             image_counter: 0,
+             carousel_type: carousel_type
     end
   end
 
