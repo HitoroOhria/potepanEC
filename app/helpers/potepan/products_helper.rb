4b5e626f724708ac947afff4451e47dc08df3400
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-22 12:27:31 +0900

    rubocopの規約に対応

--- app/helpers/potepan/products_helper.rb
+++ app/helpers/potepan/products_helper.rb
@@ -2,19 +2,20 @@ module Potepan::ProductsHelper
   def render_carousel_image(product, carousel_type: :large)
     images = product.images
     if images.present?
-      render partial:    'potepan/shared/image',
+      render partial: 'potepan/shared/image',
              collection: images,
-             locals:     { carousel_type: carousel_type }
+             locals: { carousel_type: carousel_type }
     else
-      render 'potepan/shared/image',
-             image:         Spree::Image.new,
-             image_counter: 0,
-             carousel_type: carousel_type
+      render partial: 'potepan/shared/image',
+             locals: {
+               image: Spree::Image.new,
+               image_counter: 0,
+               carousel_type: carousel_type
+             }
     end
   end
 
   def product_to_category_path(product)
-    product.taxons.present? ? potepan_category_path(product.taxons.first.id)
-                            : potepan_category_path(1)
+    product.taxons.present? ? potepan_category_path(product.taxons.first.id) : potepan_category_path(1)
   end
 end
