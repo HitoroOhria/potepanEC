7ad8195eea226dfa6266e201497ecb584da3f9df
大平 礼 <hiroto@NapolitanCoffee-Mac.local>
2020-04-24 16:36:46 +0900

    potepan_helper.rbのproduct_price(product)の内容を修正

--- app/helpers/potepan/potepan_helper.rb
+++ app/helpers/potepan/potepan_helper.rb
@@ -1,5 +1,9 @@
 module Potepan::PotepanHelper
   def product_price(product)
-    number_to_currency(product.price, format: '%n%u', unit: 'JPY', precision: 0)
+    if (jpy_price = product.prices.find { |price| price.currency == 'JPY' })
+      number_to_currency(jpy_price.amount, format: '%n%u', unit: 'JPY', precision: 0)
+    else
+      number_to_currency(product.price * 107.75, format: '%n%u', unit: 'JPY', precision: 0)
+    end
   end
 end
