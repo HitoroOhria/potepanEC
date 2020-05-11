824d413f2654a9b7f0b1dee2ad3f0b65d457ed4b
大平 礼 <hiroto100114@gmail.com>
2020-05-11 19:39:02 +0900

    [update]product_price(product)の処理をより簡潔に修正
    product.prices.find(...) -> product.master.default_priceを使う処理に変更

--- app/helpers/potepan/application_helper.rb
+++ app/helpers/potepan/application_helper.rb
@@ -1,17 +1,11 @@
 module Potepan::ApplicationHelper
   def product_price(product)
-    product_prices = product.prices
-    if (jpy_price = find_currency(product_prices, 'JPY'))
-      number_to_currency(jpy_price.amount, format: '%n%u', unit: jpy_price.currency, precision: 0)
-    elsif (usd_price = find_currency(product_prices, 'USD'))
-      number_to_currency(usd_price.amount, format: '%n%u', unit: usd_price.currency)
+    product_price = product.master.default_price
+    case product_price.currency
+    when 'JPY'
+      number_to_currency(product_price.amount, format: '%n%u', unit: product_price.currency, precision: 0)
     else
-      product_first_price = product_prices.first
-      number_to_currency(product_first_price.amount, format: '%n%u', unit: product_first_price.currency)
+      number_to_currency(product_price.amount, format: '%n%u', unit: product_price.currency)
     end
-  end
-
-  def find_currency(product_prices, currency)
-    product_prices.find { |price| price.currency == currency }
   end
 end
--- spec/helpers/potepan/application_helper_spec.rb
+++ spec/helpers/potepan/application_helper_spec.rb
@@ -7,50 +7,20 @@ RSpec.describe "helpers/potepan/application_helper.rb", type: :helper do
   describe 'product_price(product)' do
     subject { product_price(product) }
 
-    context 'product.pricesに、currency属性が"JPY"のpriceがある時' do
+    context 'product.master.defaul_priceの、currency属性が"JPY"の時' do
       before do
-        product.prices.first.update_attributes(amount: 1980, currency: 'JPY')
+        product.master.default_price.update_attributes(amount: rand(500000), currency: 'JPY')
       end
 
-      it { should eq '1,980JPY' }
+      it { should match /.*\d{1,3}JPY/ }
     end
 
-    context 'product.pricesに、currency属性が"JPY"のpriceがなく、currency属性が"USD"のpriceがある時' do
+    context 'product.master.defaul_priceの、currency属性が"JPY"以外の時' do
       before do
-        product.prices.first.update_attributes(amount: 1075.99, currency: 'USD')
+        product.master.default_price.update_attributes(amount: rand(500000), currency: 'USD')
       end
 
-      it { should eq '1,075.99USD' }
-    end
-
-    context 'product.pricesに、priceのcurrency属性が"JPY"も"USD"もない時' do
-      before do
-        product.prices.first.update_attributes(amount: 2057.99, currency: 'EUR')
-        product.prices.create(attributes_for(:price, amount: 37.45, currency: 'ARI'))
-      end
-
-      it { should eq '2,057.99EUR' }
-    end
-  end
-
-  describe 'find_currency(product_prices, currency)' do
-    context '引数 :currency に指定した通貨単位が、product.pricesのcurrency属性の中にあるとき' do
-      let(:price_usd)  { product.prices.find_by(currency: 'USD') }
-      let!(:price_eur) { variant.prices.create(attributes_for(:price, currency: 'EUR')) }
-
-      it '引数 currency に"USD"を指定すると、product.pricesの中から、currency属性が"USD"のpriceを返すこと' do
-        expect(find_currency(product.prices, 'USD')).to eq price_usd
-      end
-
-      it '引数 currency に"EUR"を指定すると、product.pricesの中から、currency属性が"EUR"のpriceを返すこと' do
-        expect(find_currency(product.prices, 'EUR')).to eq price_eur
-      end
-    end
-
-    context '引数 currency に指定した通貨単位が、product.pricesのcurrency属性の中にないとき' do
-      it '引数 currency に"HOG"を指定すると、nilを返すこと' do
-        expect(find_currency(product.prices, 'HOG')).to eq nil
-      end
+      it { should match /.*\d{1,3}.\d{2}[A-Z]{3}/ }
     end
   end
 end
