0c9ef4cd61fe6999f81f22223265da0a63e0ca9e
大平 礼 <hiroto100114@gmail.com>
2020-05-13 13:34:49 +0900

    [clean] potepan/categories_helper#take_option_values(option_type_name)のspecを理解しやすいように修正
      let(:option_type):
        - create(:option_type) -> create(:option_type, name: 'Brand')
    
      it '引数にSpree::OptionTypeのname属性をStringで渡すと...' do expect():
        - option_values(option_type.name)
        - option_values('Brand')

--- spec/helpers/potepan/categories_helper_spec.rb
+++ spec/helpers/potepan/categories_helper_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "helpers/potepan/categories_helper.rb", type: :helper do
-  let(:option_type) { create(:option_type) }
-  before do
-    2.times { option_type.option_values.create(attributes_for(:option_value)) }
-  end
-
   describe 'take_option_values(option_type_name)' do
+    let(:option_type) { create(:option_type, name: 'Brand') }
+
+    before do
+      2.times { option_type.option_values.create(attributes_for(:option_value)) }
+    end
+
     it '引数にSpree::OptionTypeのname属性をStringで渡すと、該当するSpree::OptionTypteモデルの.option_valuesを返す' do
-      expect(option_values(option_type.name)).to eq option_type.option_values
+      expect(option_values('Brand')).to eq option_type.option_values
     end
   end
 end
