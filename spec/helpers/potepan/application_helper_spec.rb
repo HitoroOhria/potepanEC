2baa34bbdc3331dff40e7707359c96289e1aa549
大平 礼 <hiroto100114@gmail.com>
2020-05-13 13:32:42 +0900

    [add]potepan/application_helper.rbのproduct_price(product)に価格の桁数に対するspecを追加
      context:
        - product.master.defaul_priceの、amount属性の数値が4桁未満の時
        - product.master.defaul_priceの、amount属性の数値が4桁以上の時

--- spec/helpers/potepan/application_helper_spec.rb
+++ spec/helpers/potepan/application_helper_spec.rb
@@ -12,7 +12,29 @@ RSpec.describe "helpers/potepan/application_helper.rb", type: :helper do
         product.master.default_price.update_attributes(amount: rand(500_000), currency: 'JPY')
       end
 
-      it { should match(/.*\d{1,3}JPY/) }
+      it '"○○○JPY"のように、数字の後に"JPY"を付けた文字列を返す' do
+        should match(/\d+JPY/)
+      end
+
+      context 'product.master.defaul_priceの、amount属性の数値が4桁未満の時、' do
+        before do
+          product.master.default_price.update_attributes(amount: rand(999), currency: 'JPY')
+        end
+
+        it '"◯◯◯"のように、数字部分にカンマを含まない' do
+          should_not include(',')
+        end
+      end
+
+      context 'product.master.defaul_priceの、amount属性の数値が4桁以上の時、' do
+        before do
+          product.master.default_price.update_attributes(amount: rand(1000..500_000), currency: 'JPY')
+        end
+
+        it '"◯◯,◯◯◯"のように、数字部分を3桁毎にカンマで区切った文字列を返す' do
+            should match(/\d+,\d{3}/)
+        end
+      end
     end
 
     context 'product.master.defaul_priceの、currency属性が"JPY"以外の時' do
@@ -20,7 +42,29 @@ RSpec.describe "helpers/potepan/application_helper.rb", type: :helper do
         product.master.default_price.update_attributes(amount: rand(500_000), currency: 'USD')
       end
 
-      it { should match(/.*\d{1,3}.\d{2}[A-Z]{3}/) }
+      it '"○○○#{currency}"のように、数字の後にcurrencyの文字列を追加した文字列を返す' do
+        should match(/\d+USD/)
+      end
+
+      context 'product.master.defaul_priceの、amount属性の数値が4桁未満の時、' do
+        before do
+          product.master.default_price.update_attributes(amount: rand(999), currency: 'JPY')
+        end
+
+        it '"◯◯◯"のように、数字部分にカンマを含まない' do
+          should_not include(',')
+        end
+      end
+
+      context 'product.master.defaul_priceの、amount属性の数値が4桁以上の時、' do
+        before do
+          product.master.default_price.update_attributes(amount: rand(1000..500_000), currency: 'JPY')
+        end
+
+        it '"◯◯,◯◯◯"のように、数字部分を3桁毎にカンマで区切った文字列を返す' do
+          should match(/\d+,\d{3}/)
+        end
+      end
     end
   end
 end
