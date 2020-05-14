c48cbd1e569ed93e67a356953a4c02cd8c68a1e3
大平 礼 <hiroto100114@gmail.com>
2020-05-14 20:10:39 +0900

    [remove]可動性の観点から、2つのsherd_contextを削除
      - product_by_root_and_child_setup.rb
      - taxonomy_and_taxon_by_root_and_child_setup.rb

--- spec/support/conext/taxonomy_and_taxon_by_root_and_child_setup.rb
+++
@@ -1,9 +0,0 @@
-RSpec.shared_context "taxonomy and taxon by root and child setup" do
-  let!(:taxonomy)    { create(:taxonomy) }
-  let!(:taxon_root)  { taxonomy.root }
-  let!(:taxon_child) {
-    taxon_root.children.create(
-      attributes_for(:taxon, taxonomy_id: taxonomy.id)
-    )
-  }
-end
