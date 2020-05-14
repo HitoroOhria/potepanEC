f668d620f4e7a0fc69ddd4bbd80425b6ce274db4
大平 礼 <hiroto100114@gmail.com>
2020-05-14 19:33:14 +0900

    [remove][add]option_values(option_type_name)をヘルパーからコントローラに移動
      [remove]:
        - app/helpers/potepan/categories_helper.rb
      [add]:
        - potepan/categories_controller.rb -> option_values(option_type_name)

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,5 +3,13 @@ class Potepan::CategoriesController < ApplicationController
     @taxon = Spree::Taxon.find(params[:taxon_id])
     @products = @taxon.all_products.includes(master: :default_price)
     @taxonomies = Spree::Taxonomy.all.includes(:root)
+    @size_option_values = option_values('tshirt-size')
+    @color_option_values = option_values('tshirt-color')
+  end
+
+  private
+
+  def option_values(option_type_name)
+    Spree::OptionType.includes(:option_values).find_by(name: option_type_name).option_values
   end
 end
--- app/helpers/potepan/categories_helper.rb
+++
@@ -1,5 +0,0 @@
-module Potepan::CategoriesHelper
-  def option_values(option_type_name)
-    Spree::OptionType.includes(:option_values).find_by(name: option_type_name).option_values
-  end
-end
