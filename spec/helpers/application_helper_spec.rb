c95ed548204ae605ebd344b061f68157f35acf5d
大平 礼 <hiroto100114@gmail.com>
2020-05-14 20:30:02 +0900

    [clean]application_helper_spec.rbで、より見やすくなるようにリファクタリング
      - contextに条件部分の記載を移動

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "helpers/application_helper.rb", type: :helper do
   describe 'full_title(page_title)' do
-    it "文字列を引数に取ると、ベースタイトルと結合した文字列を返す" do
-      expect(full_title('bags')).to eq 'bags | BIGBAG store'
+    context '引数がStringの時' do
+      it 'ベースタイトルと結合した文字列を返す' do
+        expect(full_title('bags')).to eq 'bags | BIGBAG store'
+      end
     end
 
-    it "数字を引数に取ると、ベースタイトルと結合した文字列を返す" do
-      expect(full_title(123)).to eq '123 | BIGBAG store'
+    context '引数がIntegerの時' do
+      it 'ベースタイトルと結合した文字列を返す' do
+        expect(full_title(123)).to eq '123 | BIGBAG store'
+      end
     end
 
-    it "空白文字を引数に取ると、ベースタイトルのみの文字列を返す" do
-      expect(full_title('')).to eq 'BIGBAG store'
+    context '引数が空文字の時' do
+      it 'ベースタイトルのみの文字列を返す' do
+        expect(full_title('')).to eq 'BIGBAG store'
+      end
     end
 
-    it "nilを引数に取ると、ベースタイトルのみの文字列を返す" do
-      expect(full_title(nil)).to eq 'BIGBAG store'
+    context '引数がnilの時' do
+      it 'ベースタイトルのみの文字列を返す' do
+        expect(full_title(nil)).to eq 'BIGBAG store'
+      end
     end
   end
 end
