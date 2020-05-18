9055f4aea7b8736be17c0f175de145340afc9b9e
大平 礼 <hiroto100114@gmail.com>
2020-05-18 12:10:47 +0900

    [update]プロダクトの価格が、仕様通りに表示されるように修正 [clean]各区表示のロジックを、Money.new.formatを仕様するようにリファクタリング
      - potepan/application_helper.rb
      - potepan/application_helper_spec.rb

--- app/helpers/potepan/application_helper.rb
+++ app/helpers/potepan/application_helper.rb
@@ -1,14 +1,6 @@
 module Potepan::ApplicationHelper
   def product_price(product)
     product_price = product.master.default_price
-    case product_price.currency
-    when 'JPY'
-      number_to_currency(product_price.amount, format: '%n%u',
-                                               unit: product_price.currency,
-                                               precision: 0)
-    else
-      number_to_currency(product_price.amount, format: '%n%u',
-                                               unit: product_price.currency)
-    end
+    Spree::Money.parse(product_price.amount, product_price.currency).format
   end
 end
--- spec/helpers/potepan/application_helper_spec.rb
+++ spec/helpers/potepan/application_helper_spec.rb
@@ -2,68 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "helpers/potepan/application_helper.rb", type: :helper do
   let(:product) { create(:product) }
-  let(:variant) { create(:variant, product: product) }
+  let(:variant) { product.master }
 
   describe '#product_price(product)' do
+    before do
+      variant.default_price.update_attributes(amount: rand(500_000), currency: 'USD')
+    end
+
     subject { product_price(product) }
 
-    context 'product.master.defaul_priceの、currency属性が"JPY"の時' do
+    it '"$◯◯◯"のように、数字の前に価格記号を付けた文字列を返す' do
+      is_expected.to match(/$\d+/)
+    end
+
+    context 'product.master.defaul_priceの、amount属性の数値が4桁未満の時' do
       before do
-        product.master.default_price.update_attributes(amount: rand(500_000), currency: 'JPY')
+        product.master.default_price.update_attributes(amount: rand(999))
       end
 
-      it '"○○○JPY"のように、数字の後に"JPY"を付けた文字列を返す' do
-        is_expected.to match(/\d+JPY/)
-      end
-
-      context 'product.master.defaul_priceの、amount属性の数値が4桁未満の時' do
-        before do
-          product.master.default_price.update_attributes(amount: rand(999), currency: 'JPY')
-        end
-
-        it '"◯◯◯"のように、数字部分にカンマを含まない' do
-          is_expected.to_not include(',')
-        end
-      end
-
-      context 'product.master.defaul_priceの、amount属性の数値が4桁以上の時' do
-        before do
-          product.master.default_price.update_attributes(amount: rand(1000..500_000), currency: 'JPY')
-        end
-
-        it '"◯◯,◯◯◯"のように、数字部分を3桁毎にカンマで区切った文字列を返す' do
-          is_expected.to match(/\d+,\d{3}/)
-        end
+      it '"◯◯◯"のように、数字部分にカンマを含まない' do
+        is_expected.to_not include(',')
       end
     end
 
-    context 'product.master.defaul_priceの、currency属性が"JPY"以外の時' do
+    context 'product.master.defaul_priceの、amount属性の数値が4桁以上の時' do
       before do
-        product.master.default_price.update_attributes(amount: rand(500_000), currency: 'USD')
+        product.master.default_price.update_attributes(amount: rand(1000..500_000))
       end
 
-      it '"○○◯CUR"のように、数字の後にcurrency属性の値を追加した文字列を返す' do
-        is_expected.to match(/\d+USD/)
-      end
-
-      context 'product.master.defaul_priceの、amount属性の数値が4桁未満の時' do
-        before do
-          product.master.default_price.update_attributes(amount: rand(999), currency: 'JPY')
-        end
-
-        it '"◯◯◯"のように、数字部分にカンマを含まない' do
-          is_expected.to_not include(',')
-        end
-      end
-
-      context 'product.master.defaul_priceの、amount属性の数値が4桁以上の時' do
-        before do
-          product.master.default_price.update_attributes(amount: rand(1000..500_000), currency: 'JPY')
-        end
-
-        it '"◯◯,◯◯◯"のように、数字部分を3桁毎にカンマで区切った文字列を返す' do
-          is_expected.to match(/\d+,\d{3}/)
-        end
+      it '"◯◯,◯◯◯"のように、数字部分を3桁毎にカンマで区切った文字列を返す' do
+        is_expected.to match(/\d{1,3},\d{3}/)
       end
     end
   end
