ed9ed00423692f8c84f0578cafc5a7b7663cfcfa
大平 礼 <hiroto100114@gmail.com>
2020-05-26 19:36:18 +0900

    [add]Productsコントローラーに関連商品に関する定数とインスタンス変数を追加
      - RELATION_PRODUCTS_COUNT
      - @relation_products

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,8 @@
 class Potepan::ProductsController < ApplicationController
+  RELATION_PRODUCTS_COUNT = 4
+
   def show
     @product = Spree::Product.find(params[:product_id])
+    @relation_products = @product.relation_products.shuffle[0...RELATION_PRODUCTS_COUNT]
   end
 end
