b7f7c6da20e0b8a8817885be5739ec749eece5fa
大平 礼 <hiroto100114@gmail.com>
2020-05-26 21:31:18 +0900

    [fix]Productコントローラーのテストを修正

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Potepan::ProductsController, type: :controller do
 
     subject { response }
 
-    context 'ビューレンダリング' do
+    context 'レスポンス' do
       before do
         get :show, params: { product_id: product.id }
       end
@@ -29,8 +29,8 @@ RSpec.describe Potepan::ProductsController, type: :controller do
 
     context '@relation_products' do
       context '関連商品のランダム性' do
-        let(:taxon)   { create(:taxon) }
-        let(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon)   { create(:taxon) }
+        let!(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
         let(:first_relation_products) { assigns(:relation_products) }
         let(:second_relation_products) { assigns(:relation_products) }
@@ -51,8 +51,8 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       end
 
       context '関連商品の表示内容' do
-        let(:taxon)   { create(:taxon) }
-        let(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon)   { create(:taxon) }
+        let!(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
         subject(:relation_products) { assigns(:relation_products) }
 
@@ -125,8 +125,8 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       end
 
       context '関連商品の対象' do
-        let(:taxon) { create(:taxon) }
-        let(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
+        let!(:taxon) { create(:taxon) }
+        let!(:product) { taxon.products.create(attributes_for(:product, shipping_category_id: 1)) }
 
         subject(:relation_products) { assigns(:relation_products) }
 
@@ -144,58 +144,37 @@ RSpec.describe Potepan::ProductsController, type: :controller do
           end
 
           it '@relation_productsはランダムな商品である' do
-            expect(first_relation_products).to_not eq second_relation_products
+            expect(first_relation_products.map(&:id)).to_not eq second_relation_products.map(&:id)
           end
         end
 
         context '@productが所属しているTaxonが1個の時' do
-          context 'Taxonが親ノードの時' do
-            let(:product_attributes)   { attributes_for(:product, shipping_category_id: 1) }
+          let(:product_attributes1)  { attributes_for(:product, shipping_category_id: 1) }
+          let(:product_attributes2)  { attributes_for(:product, shipping_category_id: 1) }
+          let(:product_attributes3)  { attributes_for(:product, shipping_category_id: 1) }
+          let(:product_attributes4)  { attributes_for(:product, shipping_category_id: 1) }
+          let(:product_attributes5)  { attributes_for(:product, shipping_category_id: 1) }
 
-            let!(:child_taxon)         { taxon.children.create(attributes_for(:taxon)) }
-            let!(:child_taxon_product) { child_taxon.products.create(product_attributes) }
+          context 'Taxonが親ノードの時' do
+            let!(:child_taxon)          { taxon.children.create(attributes_for(:taxon)) }
+            let!(:other_taxon)          { create(:taxon) }
+            let!(:taxon_other_product1) { taxon.products.create(product_attributes1) }
+            let!(:taxon_other_product2) { taxon.products.create(product_attributes2) }
+            let!(:child_taxon_product1) { child_taxon.products.create(product_attributes3) }
+            let!(:child_taxon_product2) { child_taxon.products.create(product_attributes4) }
+            let!(:other_taxon_product)  { other_taxon.products.create(product_attributes5) }
 
             before do
+              2.times { create(:product) }
               get :show, params: { product_id: product.id }
             end
 
-            it '親ノードとその子孫ノードに属する全ての商品が対象である' do
-              is_expected.to include product
-              is_expected.to include child_taxon_product
-            end
-          end
-
-          context 'Taxonが中間ノードの時' do
-            let(:product_attributes1)   { attributes_for(:product, shipping_category_id: 1) }
-            let(:product_attributes2)   { attributes_for(:product, shipping_category_id: 1) }
-
-            let!(:middle_taxon)         { taxon.children.create(attributes_for(:taxon)) }
-            let!(:leaf_taxon)           { middle_taxon.children.create(attributes_for(:taxon)) }
-            let!(:middle_taxon_product) { middle_taxon.products.create(product_attributes1) }
-            let!(:leaf_taxon_product)   { leaf_taxon.products.create(product_attributes2) }
-
-            before do
-              get :show, params: { product_id: middle_taxon_product.id }
-            end
-
-            it '内部ノードとその子孫ノードに属する全ての商品が対象である' do
-              is_expected.to include middle_taxon_product
-              is_expected.to include leaf_taxon_product
-            end
-          end
-
-          context 'Taxonが葉ノードの時' do
-            let(:product_attributes)   { attributes_for(:product, shipping_category_id: 1) }
-
-            let!(:child_taxon)         { taxon.children.create(attributes_for(:taxon)) }
-            let!(:child_taxon_product) { child_taxon.products.create(product_attributes) }
-
-            before do
-              get :show, params: { product_id: child_taxon_product.id }
-            end
-
-            it '葉ノードに属する全ての商品が対象である' do
-              is_expected.to include child_taxon_product
+            it '親ノードとその子孫ノードに属する商品であること' do
+              is_expected.to_not include other_taxon_product
+              is_expected.to     include taxon_other_product1
+              is_expected.to     include taxon_other_product2
+              is_expected.to     include child_taxon_product1
+              is_expected.to     include child_taxon_product2
             end
           end
         end
